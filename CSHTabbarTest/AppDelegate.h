0d3adf3abd82dad69d905421ffbadb0cc7b71991
csh <csh@criusdeimac.local>
2016-06-30 17:24:48 +0800

    Initial Commit

---
+++ CSHTabbarTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CSHTabbarTest
+//
+//  Created by csh on 16/6/30.
+//  Copyright © 2016年 XDJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
